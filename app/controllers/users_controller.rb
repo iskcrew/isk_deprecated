0e4598aebca94781f2d0c062484910f66f4bacf3
Vesa-Pekka Palmu <vpalmu@depili.fi>
2013-06-08 21:36:55 +0300

    Käyttäjiä voi luoda lisää

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,16 @@
 class UsersController < ApplicationController
-  
+
   before_filter :require_admin
-  
+
   def index
     @users = User.all
   end
-  
+
   def roles
     @user = User.find(params[:id])
     @roles = Role.all
   end
-  
+
   #Grant roles to a user
   def grant
     user=User.find(params[:id])
@@ -20,7 +20,7 @@ class UsersController < ApplicationController
         unless user.roles.include?(r)
           user.roles << r
         end
-      
+
       else
         if user.roles.include?(r)
           user.roles.delete(r)
@@ -30,42 +30,46 @@ class UsersController < ApplicationController
     user.save!
     flash[:notice] = "User roles changed"
     redirect_to :action=>'index'
-    
+
   end
-  
+
   def list
     @users = User.find(:all)
   end
-  
+
   def new
     @edituser=User.new
   end
-  
+
   def create
-    @edituser=User.new(params[:edituser])
-    if params[:password][:password] == params[:password][:verify] then
-      @edituser.password=params[:password][:password]
-      if @edituser.save then
-        flash[:notice]="User created"
-        redirect_to :controller=>"users",:action=>"list"
+    User.transaction do 
+      @user=User.new
+      @user.username = params[:user][:username]
+      if params[:password][:password] == params[:password][:verify] then
+        @user.password=params[:password][:password]
+        if @user.save then
+          flash[:notice]="User created"
+          redirect_to :controller=>"users",:action=>"index"
+        else
+          render :action=>"new"
+        end
       else
+        flash[:error]="Passwords don't match"
+        @edituser.errors.add "Passwords don't match",""
         render :action=>"new"
       end
-    else
-      flash[:error]="Passwords don't match"
-      @edituser.errors.add "Passwords don't match",""
-      render :action=>"new"
+
     end
   end
-    
+
   def show
     @user=User.find(params[:id])
   end
-  
+
   def edit
     @edituser=User.find(params[:id])
   end
-  
+
   def update
     @edituser=User.find(params[:id])
     @edituser.attributes=params[:user]
@@ -85,14 +89,14 @@ class UsersController < ApplicationController
       render :action=>"edit"
     end
   end
-  
+
   private
-  
+
   def require_admin
     unless current_user.has_role?('admin')
       raise ApplicationController::PermissionDenied
     end
   end
-    
-  
+
+
 end
