0b66ed6ad5ee5a57b87f2da4c7209f3d2c0db344
Vesa-Pekka Palmu <vpalmu@depili.fi>
2013-06-10 16:02:38 +0300

    kesä

--- app/models/master_group.rb
+++ app/models/master_group.rb
@@ -12,7 +12,9 @@ class MasterGroup < ActiveRecord::Base
   
   
   scope :orphan, joins('LEFT OUTER JOIN groups on master_groups.id = groups.master_group_id').where('groups.id IS NULL and master_groups.id <> ?', MasterGroup::Ungrouped_id)
-  scope :defined_groups, where('id != ?', MasterGroup::Ungrouped_id).where(:event_id => 2).order('name')
+
+TODO: eventtikäsittely
+  scope :defined_groups, where('id != ?', MasterGroup::Ungrouped_id).where(:event_id => 1).order('name')
   scope :ungrouped, find(MasterGroup::Ungrouped_id)
   
   
