872ec2050256656e478738db45ae5a57145e0a92
Vesa-Pekka Palmu <vpalmu@depili.fi>
2013-06-27 09:57:49 +0300

    muutettu kommunikaatiota niin ettei lähetetä tupla-enkoodattua jsonia

--- app/controllers/iskdpy_controller.rb
+++ app/controllers/iskdpy_controller.rb
@@ -1,16 +1,16 @@
 class IskdpyController < WebsocketRails::BaseController
   def current_slide
       d = Display.find(message)
-      send_message :current_slide, {:current_slide => d.current_slide.to_json}
+      send_message :current_slide, d.current_slide.to_hash(d.presentation.duration)
   end
   
   def presentation
     d = Display.find(message)
-    send_message :current_presentatin, {:current_presentation => d.presentation.to_json}
+    send_message :current_presentatin, d.presentation.to_hash
   end
   
   def display
     d = Display.find(message)
-    send_message :display_data, {:display_data => d.to_json}
+    send_message :display_data, d.to_hash
   end
 end
