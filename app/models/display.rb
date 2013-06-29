da2c0a12b27f17c352579cc29ee47a0efebec37f
isk <isk@isk-server.depili.fi>
2013-06-29 14:13:13 +0300

    listätty id näyttimen serialisaatioon

--- app/models/display.rb
+++ app/models/display.rb
@@ -81,6 +81,7 @@ class Display < ActiveRecord::Base
   def to_hash
     h = Hash.new
     h[:metadata_updated_at] = self.metadata_updated_at.to_i
+    h[:id] = self.id
     h[:last_contact_at] = self.last_contact_at.to_i
     h[:updated_at] = self.updated_at.to_i
     h[:created_at] = self.created_at.to_i
