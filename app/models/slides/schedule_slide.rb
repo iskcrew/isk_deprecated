11bff4e4449483b15432e746a3b7c7b2952310d9
Vesa-Pekka Palmu <vpalmu@depili.fi>
2013-07-04 21:00:31 +0300

    Luotu schedule_slide STI-kelmuna sisältämään automaattisesti generoidut aikataulukelmut

---
+++ app/models/slides/schedule_slide.rb
@@ -0,0 +1,13 @@
+class ScheduleSlide < Slide
+  #Automatically generated schedule slide
+  TypeString = 'schedule'
+  
+
+  def initialize
+    super
+    self.is_svg = true
+    self.ready = false
+  end
+  
+  
+end
