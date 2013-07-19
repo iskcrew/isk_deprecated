befe1c80884c1a5f3f0830590c82fa38aba4d34d
Vesa-Pekka Palmu <vpalmu@depili.fi>
2013-07-19 22:18:19 +0300

    Lisätty jquerry-waypoints js-kirjasto

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,8 @@
 //= require jquery_ujs
 //= require jquery.ui.all
 //= require jquery.timer
+//= require waypoints
+//= require waypoints-sticky
 //= require websocket_rails/main
 //= require_tree .
 
