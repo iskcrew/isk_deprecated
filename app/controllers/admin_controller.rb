293adc22ee6d9e61409777b528bec1e19b2e20c8
Vesa-Pekka Palmu <vpalmu@depili.fi>
2013-07-28 01:18:17 +0300

    Add scaffolding for planned actions in admincontroller

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,2 +1,17 @@
 class AdminController < ApplicationController
+	
+	def index
+		
+	end
+	
+	def clear_cache
+		cache_store.clear
+		redirect_to :index
+	end
+	
+	def regenerate_slide_images
+		Slide.all.each {|s| s.delay.generate_images}
+		redirect_to :index
+	end
+	
 end
