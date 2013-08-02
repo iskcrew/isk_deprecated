9957aea93fda5c2b36f33ead489ba2fba038e7d1
Vesa-Pekka Palmu <vpalmu@depili.fi>
2013-08-02 21:07:00 +0300

    Better logging

--- script/background_jobs.rb
+++ script/background_jobs.rb
@@ -7,15 +7,23 @@ puts 'Starting ISK server background process'
 
 Daemon.daemonize(Rails.root.join('tmp','pids','background_jobs.pid'),Rails.root.join('log', 'background_jobs.log'))
 
-puts Time.now.to_s + " Daemon started"
+stamped_puts Time.now.to_s + " Daemon started"
 
 ActiveRecord::Base.connection.reconnect!
 loop do
+	stamped_puts 'Fetching http-slides'
 	HttpSlide.all.each do |slide|
 		slide.delay.fetch!
 	end
+	
+	stamped_puts 'Generating schedule slides'
 	Schedule.all.each do |schedule|
 		schedule.generate_slides
 	end
 	sleep(60)
+end
+
+def stamped_puts(str)
+	puts Time.now.to_s + " " + __FILE__ + ":" + __LINE__ + " " + str
+	
 end
