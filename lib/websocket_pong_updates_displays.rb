91b98112cc3cf8e43524303316df5a0f6393db5f
Vesa-Pekka Palmu <vpalmu@depili.fi>
2014-01-09 23:49:30 +0200

    BUGFIX: this also needed updating for the displaystate system

--- lib/websocket_pong_updates_displays.rb
+++ lib/websocket_pong_updates_displays.rb
@@ -12,7 +12,7 @@ module  WebsocketRails
   InternalController.class_eval do
     def do_pong
       connection.pong = true
-      Display.where(:websocket_connection_id => connection.id).update_all(:last_contact_at => Time.now)
+      DisplayState.where(:websocket_connection_id => connection.id).update_all(:last_contact_at => Time.now)
     end
   end
 end
