d914bde6ee727fcfc694bf9c7b9778f975e70a51
Vesa-Pekka Palmu <vpalmu@depili.fi>
2014-01-13 22:29:28 +0200

    Indentation was a mess of spaces and tabs...

--- app/assets/javascripts/websocket_updates.js
+++ app/assets/javascripts/websocket_updates.js
@@ -1,28 +1,28 @@
 $().ready(function() {
 	
 	$('#scrollingDiv').waypoint('sticky', {
-	  wrapper: '<div class="scrollingDiv-wrapper" />',
-	  stuckClass: 'stuck'
+		wrapper: '<div class="scrollingDiv-wrapper" />',
+		stuckClass: 'stuck'
 	});
 	
 	var dispatcher = new WebSocketRails(window.location.host + '/websocket');
 
 	function replace_slideitem(slide) {
-	  	console.log('Updating slide data: ' + window.location.protocol + "/slides/" + slide.id);
+		console.log('Updating slide data: ' + window.location.protocol + "/slides/" + slide.id);
 		
 		//Tarkistetaan että onko kelmua hötömölössä ennen kuin haetaan ajaxia
 		if ($('div#slide_' + slide.id).length == 0) return
 		
 		$.ajax({
-		  type: "GET",
-		  url: window.location.origin + "/slides/" + slide.id,
-		  dataType: 'script'
+			type: "GET",
+			url: window.location.origin + "/slides/" + slide.id,
+			dataType: 'script'
 		});
 	};
 
 	function replace_slide_image(slide) {
 		console.log('Updating slide images for slide id: ' + slide.id);
-	  	$('img#slide_full_' + slide.id).each(function(index, element) {
+		$('img#slide_full_' + slide.id).each(function(index, element) {
 			console.log(' >Found full size images..');
 			$(element).attr("src", window.location.origin + '/slides/' + slide.id + '/full?t=' + slide.images_updated_at);
 		});
@@ -38,19 +38,19 @@ $().ready(function() {
 		});
 	};
 
-    function update_display(display) {
-        console.log('Updating display id: ' + display.id);
+	function update_display(display) {
+		console.log('Updating display id: ' + display.id);
 		if ($('#display_' + display.id).length == 0) return
 		
 		$.ajax({
-		  type: "GET",
-		  url: window.location.origin + "/displays/" + display.id,
-		  dataType: 'script'
+			type: "GET",
+			url: window.location.origin + "/displays/" + display.id,
+			dataType: 'script'
 		});
-    };
-    
-    displays = dispatcher.subscribe('display');
-    displays.bind('update', update_display);
+	};
+	
+	displays = dispatcher.subscribe('display');
+	displays.bind('update', update_display);
 
 	slidelist = dispatcher.subscribe('slide');
 	slidelist.bind('update', replace_slideitem);
