0285963db2d4d460511be11659c9811ae9ae1fda
Vesa-Pekka Palmu <vpalmu@depili.fi>
2014-01-13 23:07:25 +0200

    Fixed miss-match of tab/space indentation

--- app/models/websocket_notifications.rb
+++ app/models/websocket_notifications.rb
@@ -1,30 +1,30 @@
 # ISK - A web controllable slideshow system
 #
-# Author::    Vesa-Pekka Palmu
+# Author::		Vesa-Pekka Palmu
 # Copyright:: Copyright (c) 2012-2013 Vesa-Pekka Palmu
-# License::   Licensed under GPL v3, see LICENSE.md
+# License::		Licensed under GPL v3, see LICENSE.md
 
 
 class WebsocketNotifications < ActiveRecord::Observer
-  #TODO näyttimelle kunnolla observointia...
-  observe :slide, :master_group, :group, :presentation, :display, :override_queue
-  	
+	#TODO näyttimelle kunnolla observointia...
+	observe :slide, :master_group, :group, :presentation, :display, :override_queue
+		
 	def after_commit(obj)
-    Rails.logger.debug '-> Prosessing after_commit callback...'
+		Rails.logger.debug '-> Prosessing after_commit callback...'
 		if obj.created_at == obj.updated_at
-			Rails.logger.debug '  -> new record!'
+			Rails.logger.debug '	-> new record!'
 			event = :create
 		else
-			Rails.logger.debug '  -> update'
+			Rails.logger.debug '	-> update'
 			event = :update
 		end
 		
 		data = {:id => obj.id}
-    
 		
-    trigger obj, event, data
-    display_datas(obj)
-    
+		
+		trigger obj, event, data
+		display_datas(obj)
+		
 		if obj.changed.include?('images_updated_at') && event == :update
 			Rails.logger.debug "-> Slide image has been updated, sendin notifications"
 			obj.updated_image_notifications
@@ -32,21 +32,21 @@ class WebsocketNotifications < ActiveRecord::Observer
 	end
 
 
-  private
+	private
 
-  def trigger(obj, event, data)
-    WebsocketRails[get_channel(obj)].trigger(event, data)
-  end
+	def trigger(obj, event, data)
+		WebsocketRails[get_channel(obj)].trigger(event, data)
+	end
 
-  def get_channel(obj)
-    return obj.class.base_class.name.downcase
-  end
+	def get_channel(obj)
+		return obj.class.base_class.name.downcase
+	end
 
-  def display_datas(obj)
-    obj.displays.each do |d|
-      data = d.to_hash
-      WebsocketRails[d.websocket_channel].trigger(:data, data)
-    end
-  end
+	def display_datas(obj)
+		obj.displays.each do |d|
+			data = d.to_hash
+			WebsocketRails[d.websocket_channel].trigger(:data, data)
+		end
+	end
 
 end
