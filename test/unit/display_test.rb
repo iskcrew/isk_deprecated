c2e8da98f465e8c01747a929dfcd6ad519d28cad
Vesa-Pekka Palmu <vpalmu@depili.fi>
2014-01-15 18:05:20 +0200

    Initial testing for displays

--- test/unit/display_test.rb
+++ test/unit/display_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class DisplayTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "hello on existing display" do
+		d = Display.hello 'Normal', '127.0.0.1'
+		
+		assert_equal d.id, displays(:normal).id, "Got wrong display"
+		
+	end
+	
+	test "hello on new display" do
+		d = Display.hello "Hi, I'm new", "127.0.0.1"
+		
+		assert_equal 1, Display.where(name: "Hi, I'm new").count, "Display not found in db"
+	end
 end
