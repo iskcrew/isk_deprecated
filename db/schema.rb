f3d9d4745cd7909e66b1cadf7172f2ec82b1b068
Vesa-Pekka Palmu <vpalmu@depili.fi>
2014-01-17 21:04:34 +0200

    Remove accidentally added "ceremonies" table prize ceremonies got implemented as STI groups

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140108174243) do
-
-  create_table "ceremonies", :force => true do |t|
-    t.string   "name"
-    t.integer  "slidegroup_id"
-    t.integer  "event_id"
-    t.datetime "created_at",    :null => false
-    t.datetime "updated_at",    :null => false
-  end
+ActiveRecord::Schema.define(:version => 20140116112914) do
 
   create_table "delayed_jobs", :force => true do |t|
     t.integer  "priority",   :default => 0
