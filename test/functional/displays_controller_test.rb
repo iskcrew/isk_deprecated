bc3b4c316547cd6fccb19314e8309f7956ee17c6
Vesa-Pekka Palmu <vpalmu@depili.fi>
2014-01-19 21:38:07 +0200

    Initial testing for displays controller

--- test/functional/displays_controller_test.rb
+++ test/functional/displays_controller_test.rb
@@ -1,7 +1,74 @@
 require 'test_helper'
 
 class DisplaysControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	#TODO: test sorting overrides
+	
+  def setup
+		@adminsession = {user_id: users(:admin).id, username: users(:admin).username}
+		@update_data = {
+			display: {
+				name: "paranormal"
+			},
+			id: displays(:normal).id
+		}
+		
+		@add_queue_data = {
+			id: displays(:normal).id,
+			slide_id: slides(:slide_1).id
+		}
+		
+		@remove_queue_data = {
+			id: 2
+		}
+  end
+	
+	
+	test "get index" do
+		get :index, nil, @adminsession
+		
+		assert_response :success
+	end
+	
+	test "get display info" do
+		[:normal, :no_presentation, :late, :with_overrides, :manual_mode].each do |d|
+			get :show, {:id => displays(d)}, @adminsession
+			assert_response :success, "Error getting info for display: " + d.to_s
+		end
+	end
+	
+	test "get edit form" do
+		get :edit, {:id => displays(:normal)}, @adminsession
+		
+		assert_response :success
+	end
+	
+	test "update display" do
+		post :update, @update_data, @adminsession
+		
+		assert_redirected_to display_path(assigns(:display))
+	end
+	
+	test "get dpy control" do
+		get :dpy_control, {id: displays(:normal).id}, @adminsession
+		
+		assert_response :success
+	end
+	
+	test "queue slide" do
+		assert_difference "displays(:normal).override_queues.count" do
+			post :queue_slide, @add_queue_data, @adminsession
+		end
+		
+		assert_redirected_to display_path(displays(:normal).id)
+		
+	end
+	
+	test "remove override" do
+		assert_difference "displays(:with_overrides).override_queues.count", -1 do
+			post :remove_override, @remove_queue_data, @adminsession
+		end
+		
+		assert_response :redirect
+	end
+	
 end
