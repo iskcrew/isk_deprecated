f8efa6916e3a85a30bff95256120f3321ad5a0c7
Vesa-Pekka Palmu <vpalmu@depili.fi>
2014-01-21 23:42:15 +0200

    Put cache_observer first into the load order

--- config/application.rb
+++ config/application.rb
@@ -30,7 +30,7 @@ module Isk
     # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
 
     # Activate observers that should always be running.
-    config.active_record.observers = :websocket_notifications, :cache_observer
+    config.active_record.observers = :cache_observer, :websocket_notifications
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
