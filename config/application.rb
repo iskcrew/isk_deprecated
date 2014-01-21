8de9d38067b981d33176a5fa3375afceecc32b0a
Vesa-Pekka Palmu <vpalmu@depili.fi>
2014-01-21 20:36:41 +0200

    Register the new cache observer

--- config/application.rb
+++ config/application.rb
@@ -30,7 +30,7 @@ module Isk
     # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
 
     # Activate observers that should always be running.
-    config.active_record.observers = :websocket_notifications
+    config.active_record.observers = :websocket_notifications, :cache_observer
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
