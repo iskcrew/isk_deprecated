82f41a06acdc475d89fdfc886b25396bbac063fa
Vesa-Pekka Palmu <vpalmu@depili.fi>
2014-01-21 20:26:17 +0200

    Configure Cashier

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,4 +72,9 @@ Isk::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+	
+	# Cashier for better caching
+	#config.cashier.adapter = :cache_store 
+	config.cashier.adapter = :redis_store
+	
 end
