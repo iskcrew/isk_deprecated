3f30f1f5500dbb1b67b5f0f578c45f015e04dbb0
Vesa-Pekka Palmu <vpalmu@depili.fi>
2014-01-22 18:00:28 +0200

    Touch the user object when it receives new permissions

--- app/models/permission.rb
+++ app/models/permission.rb
@@ -1,11 +1,15 @@
 class Permission < ActiveRecord::Base
   
-	belongs_to :user
+	belongs_to :user, touch: true
 	belongs_to :role
 	belongs_to :slide
 	belongs_to :master_group
 	belongs_to :presentation
 	belongs_to :display
+	
+	def cache_tag
+		'permission_' + self.id.to_s
+	end
 	
 	# attr_accessible :title, :body
 end
