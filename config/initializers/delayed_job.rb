e495219cca00b57ac597bf49b4c6eb86adfc9d0a
Vesa-Pekka Palmu <vpalmu@depili.fi>
2014-02-03 19:45:06 +0200

    Saner defaults for JD
    Previously we polled the job list with 60 sleep, and
    thus generating new images for slides took too long

--- config/initializers/delayed_job.rb
+++ config/initializers/delayed_job.rb
@@ -1,8 +1,8 @@
 #Configuration for delayed_job gem
 
 Delayed::Worker.destroy_failed_jobs = false
-Delayed::Worker.sleep_delay = 60
+Delayed::Worker.sleep_delay = 5
 Delayed::Worker.max_attempts = 2
-Delayed::Worker.max_run_time = 5.minutes
+Delayed::Worker.max_run_time = 2.minutes
 Delayed::Worker.read_ahead = 10
 Delayed::Worker.delay_jobs = !Rails.env.test?
