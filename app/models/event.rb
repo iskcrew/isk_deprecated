ef7f4ee73a990171a82ccd194daff74ea5aad5ad
Vesa-Pekka Palmu <vpalmu@depili.fi>
2014-02-04 21:04:39 +0200

    Skeleton config

--- app/models/event.rb
+++ app/models/event.rb
@@ -20,6 +20,58 @@ class Event < ActiveRecord::Base
 	validates :current, :inclusion => { :in => [true, false] }	
 	validate :ensure_one_current_event
 	
+	FilePath = Rails.root.join 'data', 'events'
+	
+	#Default config for events
+	DefaultConfig = {
+		full: { # Full slide image size
+			width: 1280,
+			height: 720
+		},
+		preview: { # Preview image size
+			width: 225,
+			height: 400
+		},
+		thumb: { # Small thumbnail size
+			width: 128,
+			height: 72
+		},
+		simple_editor: {
+			heading: {
+				x: 30,
+				y: 100
+			},
+			body: {
+				y: 200,
+				margin_left: 30,
+				margin_right: 30
+			}
+		},
+		schedule: {
+			items_per_slide: 9,
+			wrap: {
+				soft: 30,
+				hard: 35
+			},
+			use_positions_from_simple: true,
+			positions: {
+				heading: {
+					x: 30,
+					y: 100,
+					font_size: 80
+				},
+				subheader: {
+					color: '#ffd700'
+				},
+				body: {
+					x: 30,
+					y: 200,
+					font_size: 48
+				}
+			}
+		}
+	}
+	
 	#After creating a new event create the associated internal slidegroups.
 	after_create do |e|
 		e.ungrouped = MasterGroup.where(:name => ('Ungrouped slides for ' + e.name)).first_or_create
@@ -56,6 +108,25 @@ class Event < ActiveRecord::Base
 		if !self.current && self.changed.include?('current')
 			errors.add(:current, "^Must have one current event")
 		end
+	end
+	
+	def config
+		return @_config ||= read_config
+	end
+
+	def read_config
+    if !self.new_record? && File.exists?(config_filename)
+      config = YAML.load(File.read(config_filename))
+		end
+		return config.blank? ? DefaultConfig : config
+	end
+	
+	def write_config
+		
+	end
+	
+	def config_filename
+		FilePath.join 'event_' + self.id.to_s + '_config.yml'
 	end
 	
 end
