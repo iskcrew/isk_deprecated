2ffb9139be07825a1352b7d7d1ad6d371b7923e5
Vesa-Pekka Palmu <vpalmu@depili.fi>
2014-02-05 13:02:01 +0200

    Use direct sql statement instead of relying on models

--- db/migrate/20140204191724_move_all_deleted_slides_to_thrash.rb
+++ db/migrate/20140204191724_move_all_deleted_slides_to_thrash.rb
@@ -6,12 +6,10 @@ class MoveAllDeletedSlidesToThrash < ActiveRecord::Migration
 	
 	
   def up
-		Slide.transaction do
-			Slide.where(deleted: true).all.each do |s|
-				e = s.master_group.event
-				s.master_group = e.thrashed
-				s.save!
-			end
+		sql = 'UPDATE slides JOIN master_groups ON slides.master_group_id = master_groups.id JOIN events ON master_groups.event_id = events.id SET slides.master_group_id = events.thrashed_id WHERE slides.deleted = 1;'
+		connection = ActiveRecord::Base.connection
+		connection.transaction do
+			connection.execute(sql)
 		end
   end
 
