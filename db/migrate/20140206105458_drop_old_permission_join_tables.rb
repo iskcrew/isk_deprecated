c7078d78578306204641300364f937024f155cdd
Vesa-Pekka Palmu <vpalmu@depili.fi>
2014-02-06 13:00:41 +0200

    Clean up old join tables

---
+++ db/migrate/20140206105458_drop_old_permission_join_tables.rb
@@ -0,0 +1,35 @@
+class DropOldPermissionJoinTables < ActiveRecord::Migration
+  def up
+		connection = ActiveRecord::Base.connection
+		connection.transaction do
+			drop_table :displays_users
+			drop_table :master_groups_users
+			drop_table :presentations_users
+			drop_table :slides_users
+		end
+  end
+
+  def down
+	  create_table "displays_users", :id => false do |t|
+	    t.integer "display_id"
+	    t.integer "user_id"
+	  end
+		
+	  create_table "master_groups_users", :id => false do |t|
+	    t.integer "master_group_id"
+	    t.integer "user_id"
+	  end
+		
+	  create_table "presentations_users", :id => false do |t|
+	    t.integer "presentation_id"
+	    t.integer "user_id"
+	  end
+		
+	  create_table "slides_users", :id => false, :force => true do |t|
+	    t.integer "slide_id"
+	    t.integer "user_id"
+	  end
+		
+		
+  end
+end
