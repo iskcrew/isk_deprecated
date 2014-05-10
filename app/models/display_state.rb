d8efbb0466183c38786fc502bc119468e58a4a18
Vesa-Pekka Palmu <vpalmu@depili.fi>
2014-05-10 22:48:33 +0300

    Remove accidental def method name end block

--- app/models/display_state.rb
+++ app/models/display_state.rb
@@ -9,11 +9,6 @@ class DisplayState < ActiveRecord::Base
 	validates :monitor, :inclusion => { :in => [true, false] }
 	validates :current_slide_id, :current_group_id, :numericality => {:only_integer => true}, :allow_nil => true
 	
-	
-	def method_name
-		
-	end
-	
 	def displays
 		[]
 	end
