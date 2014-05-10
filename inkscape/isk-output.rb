a65bcfe7cb172a24ad890f13b576779715afb962
Vesa-Pekka Palmu <vpalmu@depili.fi>
2014-05-10 18:05:00 +0300

    Comments on the inkscape plugins

--- inkscape/isk-output.rb
+++ inkscape/isk-output.rb
@@ -17,7 +17,15 @@ options.username = nil
 options.password = nil
 options.slidename = nil
 
+# Per the .inx extension definition we will get the following command-line parameters:
+# --username for the ISK login username
+# --password for the ISK password
+# --iskhost for the ISK address
+# last argument given will be the location for a file with the svg data in it.
+# inkscape also returns the selected item's svg-id with --id command line parameter
+# but we don't care about it
 
+# Parse the command line options
 OptionParser.new do |opts|
   opts.banner = "Usage: example.rb [options]"
 
@@ -41,40 +49,52 @@ OptionParser.new do |opts|
 	opts.on("-e", "--id object_id", "Object id") do |id|
 			options.id = id
 	end
-
-	
 end.parse!
 
+# Send a POST request to ISK and collect cookies
 http = Net::HTTP.new(options.host)
 resp, data = http.post('/login', "username=#{options.username}&password=#{options.password}")
 cookie = resp.response['set-cookie'].split('; ')[0]
 
+#Check the return code from the POST request
 unless resp.is_a? Net::HTTPFound
 	abort "Error loggin into ISK, aborting"
 end
 
+# Store the session cookie
 headers = {
   'Cookie' => cookie,
  }
 
+ # Read the svg data from the file provided in command line arguments
 svg_data = File.read(ARGV.last)
 
 xml = REXML::Document.new svg_data
 
+#Find the first <metadata> element in the svg document
 metadata = REXML::XPath.first( xml, "//metadata" )
 data = metadata.text
 
+# The metadata-element contains a cookie about the origin, it's in the form of slide_id!isk_host
+# so for example 123!http://example.com/ we don't care about the legacy url anymore, so just take
+# the id
+id, isk_server = data.split('!')
 
+
+# Inkscape chances the background picture from relative to absolute path, so we
+# need to change it back. The path will be something like c:/foo/bar/background/empty.png
+# and we want /backgrounds/empty, so match on backgrounds and mangle based on that.
 REXML::XPath.each( xml, '//image[@id="background_picture"]' ) do |element|
 	href = element.attribute('xlink:href').to_s
 	element.add_attribute 'xlink:href', 'backgrounds' + href.partition('backgrounds').last
 end
 
-id, isk_server = data.split('!')
-
+# Write the modified svg into a string
 post_svg = String.new
 xml.write post_svg
 
-resp, data = http.post '/slides/' << id << '/svg_data',{'svg' => post_svg}
+# post the svg data to ISK
+resp, data = http.post '/slides/' << id << '/svg_data',{'svg' => post_svg}, headers
 
+# Inkscape expects the plugin to return the svg in stdout, so do so
 puts svg_data
