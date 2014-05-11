18fb77888a176a0019645fd9ee0d5a651219e99c
Vesa-Pekka Palmu <vpalmu@depili.fi>
2014-05-11 23:28:03 +0300

    Be more robust when returning memoised slidedata

--- app/mixins/has_slidedata.rb
+++ app/mixins/has_slidedata.rb
@@ -16,7 +16,7 @@ module  HasSlidedata
   
   
   def slidedata
-		return @_slidedata if @_slidedata
+		return @_slidedata if @_slidedata.present?
     if !self.new_record? && File.exists?(self.data_filename.to_s)
       @_slidedata = YAML.load(File.read(self.data_filename))
 		end
