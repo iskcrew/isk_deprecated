fd0049b332178bcb4372a9c0143a438fa9974f6d
Vesa-Pekka Palmu <vpalmu@depili.fi>
2014-06-02 23:14:06 +0300

    Fix deprecated asset compression options

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,7 +19,9 @@ Isk::Application.configure do
   config.serve_static_assets = true
 
   # Compress JavaScripts and CSS
-  config.assets.compress = true
+  config.assets.css_compressor = :sass
+	config.assets.js_compressor = :uglifier
+
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
