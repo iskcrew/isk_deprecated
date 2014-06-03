d6709c7e67bce42ff047be48174288f95b471c22
Vesa-Pekka Palmu <vpalmu@depili.fi>
2014-06-03 23:09:39 +0300

    Remove useless debug logging

--- app/assets/javascripts/sortable.js
+++ app/assets/javascripts/sortable.js
@@ -2,11 +2,6 @@ $(document).ready(function() {
 	$("[data-sortable]").sortable({ 
 	handle : '.handle', 
 	update : function (e, ui) { 
-		console.log(ui.item);
-		console.log(ui.position);
-		console.log(ui.item.index());
-		var order = $('[data-sortable]').sortable('serialize'); 
-		
 		var data = {
 			element_id: ui.item.attr('id').split('_').pop(),
 			element_position: ui.item.index()
