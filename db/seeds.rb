300d8e46621b35985005052e2896d56634ad834d
Vesa-Pekka Palmu <vpalmu@depili.fi>
2014-06-06 19:51:29 +0300

    Bring db seeds up to date We create the effects, user roles, a current event and admin user if needed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,67 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-MasterGroup.create(:name => "Ungrouped slides", :id => 1)
-Effect.create(:name => "Random", :description => "Chooses random effect for each transition", :id => 1)
+# Effects for using with displays, the id's are part of the communication protocol.
+
+effects = [
+	[1, 'Normal', 'Default transition effect'],
+	[2, 'Subtle', 'Subtle transiton effect'],
+	[3, 'Alert',	'Attention seeking effect for alerts']
+]
+
+effects.each do |e|
+	eff = Effect.where(id: e.first).first_or_initialize
+	eff.name = e[1]
+	eff.description = e.last
+	eff.save!
+	puts "Created effect #{e[1]} with id #{eff.id}"
+end
+
+# Roles
+
+roles = [
+	['slide-hide', 'Can hide any slide.'],
+	['display-override', 'Can add slides to override queue on any display.']
+]
+
+models = [
+	Slide,
+	MasterGroup,
+	Presentation,
+	Display,
+	User
+].each do |m|
+	roles << [
+		"#{m.base_class.name.downcase}-admin",
+		"Full control for #{m.base_class.name.downcase.pluralize}"
+	]
+	roles << [
+		"#{m.base_class.name.downcase}-create",
+		"Can create new #{m.base_class.name.downcase.pluralize}"
+	]
+end
+
+roles.each do |r|
+	role = Role.where(role: r.first).first_or_initialize
+	role.description = r.last
+	role.save
+	puts "Created role #{r.first} (#{r.last}) with id #{role.id}"	
+end
+
+# Create default event if needed
+if Event.where(current: true).count != 1
+	e = Event.new
+	e.name = "Default ISK event"
+	e.current = true
+	e.save!
+	puts "Created new default event #{e.name} id #{e.id}"
+end
+
+# Create admin account
+if User.where(username: 'admin').count != 1
+	u = User.new
+	u.username = 'admin'
+	u.password = 'admin'
+	u.save!
+	puts "Created admin user (username: admin, password: admin)"
+end
