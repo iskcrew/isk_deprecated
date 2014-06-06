f6686e31314f79ffad27827e9663bb5f7ead2b15
Vesa-Pekka Palmu <vpalmu@depili.fi>
2014-06-06 16:08:27 +0300

    Migrate the slidedata mixin into rails Concern

--- app/models/concerns/has_slidedata.rb
+++ app/models/concerns/has_slidedata.rb
@@ -9,12 +9,27 @@
 
 
 module  HasSlidedata
+	# This module depends on the class setting DefaultSlidedata hash.
+	# When new data is entered it's checked against the default hash and all
+	# keys that are not also present in the default are dropped.
+	# If necessarry the values are merged so that submitting a incomplete set
+	# of data doesn't wipe others away.
+	
+	extend ActiveSupport::Concern
+	
+	
+	included do
+		# Register callbacks on the model we are included on
 
+		after_create :write_slidedata
+	end
+
+	# We need the .base_class to find the constant in the base class of the STI tree..
   def data_filename
     self.class.base_class::FilePath.join(self.filename + '_data')
   end
   
-  
+  # Read and memoize the slidedata
   def slidedata
 		return @_slidedata if @_slidedata.present?
     if !self.new_record? && File.exists?(self.data_filename.to_s)
@@ -23,23 +38,24 @@ module  HasSlidedata
 		return @_slidedata.blank? ? self.class::DefaultSlidedata : @_slidedata
   end
   
+	# Write new slidedata and sanitize the keys in it.
   def slidedata=(d)
     if d.nil?
 			d = self.class::DefaultSlidedata
 		end
 	
 	
-		# Jos jotain avainta ei ole uudessa hashissä käytetään vanhaa
-    d = slidedata.merge(d)
+		# Merge the new data with the old slidedata, if a key is in both the new contents is kept.
+		d = slidedata.merge(d)
 
-    #Heitetään ylimääräiset avaimet pois ettei tallenneta paskaa levylle
-    d.keep_if do |k, v|
+    # Sanitize the data hash, only keep keys that exist in the default hash
+		d.keep_if do |k, v|
       self.class::DefaultSlidedata.key? k
     end
   
     if d.key? :url
-      #Varmistetaan että url on ok (heittää URI::InvalidURIError jos ei ole ok)
-      URI::parse d[:url].strip
+      # Validate that the url in the :url key is valid by trying to parse it.
+			URI::parse d[:url].strip
     
       if d[:url] != self.slidedata[:url]
         @_needs_fetch = true
