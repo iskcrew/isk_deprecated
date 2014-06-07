f553a076e323c57f8a8e4c1249bdd78b6368cbf3
Vesa-Pekka Palmu <vpalmu@depili.fi>
2014-06-08 00:02:49 +0300

    Test successful and unsuccessful logins

--- test/controllers/logins_controller_test.rb
+++ test/controllers/logins_controller_test.rb
@@ -1,7 +1,38 @@
 require 'test_helper'
 
 class LoginsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+  	@login_data = {
+  		username: 'admin',
+			password: 'test1'
+  	}
+  end
+	
+	test "login" do
+  	post :create, @login_data
+		assert_redirected_to slides_path
+  end
+	
+	test "login with json" do
+		data = @login_data
+		data[:format] = :json
+		
+		post :create, data
+		
+		assert_response :success
+		json = JSON.parse @response.body
+		assert_equal 'Login successfull', json['message']
+		assert_equal users(:admin).username, json['data']['username']
+	end
+	
+	test "failed login" do
+		data = @login_data
+		data[:password] = 'wrong password'
+		
+		post :create, data
+		
+		assert_template :show
+		assert_not flash[:error].empty?
+	end
+	
 end
