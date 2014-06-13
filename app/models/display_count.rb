af067ab1cc189b9b877d901844ad5e1ec66b70fa
Vesa-Pekka Palmu <vpalmu@depili.fi>
2014-06-14 01:09:48 +0300

    Fix indentation missmatch

--- app/models/display_count.rb
+++ app/models/display_count.rb
@@ -1,17 +1,17 @@
 # ISK - A web controllable slideshow system
 #
-# Author::    Vesa-Pekka Palmu
+# Author::		Vesa-Pekka Palmu
 # Copyright:: Copyright (c) 2012-2013 Vesa-Pekka Palmu
-# License::   Licensed under GPL v3, see LICENSE.md
+# License::		Licensed under GPL v3, see LICENSE.md
 
 
 class DisplayCount < ActiveRecord::Base
-  
-  belongs_to :display
-  belongs_to :slide
-  
-  scope :by_time, -> { order(modified_at: :desc) }
-  
-  
-  
+	
+	belongs_to :display
+	belongs_to :slide
+	
+	scope :by_time, -> { order(modified_at: :desc) }
+	
+	
+	
 end
