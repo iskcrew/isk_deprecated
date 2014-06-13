005bbb4e59202c44e0535117d72c1a73c1c2a637
Vesa-Pekka Palmu <vpalmu@depili.fi>
2014-06-14 01:08:26 +0300

    Fix indentation missmatch

--- app/models/slides/schedule_slide.rb
+++ app/models/slides/schedule_slide.rb
@@ -1,34 +1,34 @@
 # ISK - A web controllable slideshow system
 #
-# Author::    Vesa-Pekka Palmu
+# Author::		Vesa-Pekka Palmu
 # Copyright:: Copyright (c) 2012-2013 Vesa-Pekka Palmu
-# License::   Licensed under GPL v3, see LICENSE.md
+# License::		Licensed under GPL v3, see LICENSE.md
 
 
 class ScheduleSlide < Slide
-  #Automatically generated schedule slide
-  TypeString = 'schedule'
-  
+	#Automatically generated schedule slide
+	TypeString = 'schedule'
+
 	before_create do |slide|
 		slide.is_svg = true
 		true
 	end
-	
+
 	private
-	
-  def rsvg_command(type)
-    command = 'cd ' << FilePath.to_s << ' && inkscape'
-    
-    if type == :full
-      command << ' -w ' << Slide::FullWidth.to_s
-      command << ' -h ' << Slide::FullHeight.to_s
-      command << ' -e ' << self.full_filename.to_s
-      command << ' ' << self.svg_filename.to_s
+
+	def rsvg_command(type)
+		command = 'cd ' << FilePath.to_s << ' && inkscape'
+
+		if type == :full
+			command << ' -w ' << Slide::FullWidth.to_s
+			command << ' -h ' << Slide::FullHeight.to_s
+			command << ' -e ' << self.full_filename.to_s
+			command << ' ' << self.svg_filename.to_s
 			command << ' >/dev/null'
-    end
-    
-    return command
-  end  
-	
-	  
+		end
+
+		return command
+	end
+
+
 end
