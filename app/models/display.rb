f388899fd9e458eaede85b09622dd42d5ae02c2d
Vesa-Pekka Palmu <vpalmu@depili.fi>
2014-06-14 21:08:03 +0300

    Clean up callbacks and validations

--- app/models/display.rb
+++ app/models/display.rb
@@ -7,25 +7,8 @@
 
 class Display < ActiveRecord::Base
 	
-	
-	before_save do
-		if self.manual
-			self.do_overrides = false
-		end
-		true
-	end
-	
-	after_save do
-		display_state.save if (display_state.changed? || display_state.new_record?)
-	end
-	
-	before_create do
-		ds = DisplayState.new
-		self.display_state = ds
-	end
-	
 	belongs_to :presentation
-	has_one :display_state
+	has_one :display_state, autosave: true
 	has_one :current_group, through: :display_state
 	has_one :current_slide, through: :display_state
 	has_many :override_queues, -> { order(:position).includes(:slide) }
@@ -34,17 +17,21 @@ class Display < ActiveRecord::Base
 	has_many :permissions
 	has_many :authorized_users, through: :permissions, source: :user, class_name: 'User'
 
-	
 	validates :name, :uniqueness => true, :presence => true, :length => { :maximum => 50 }
 	validates :manual, :inclusion => { :in => [true, false] }
+	validates :display_state, presence: true
+
+	before_save :manual_control_checks
+	before_validation :create_state, on: :create
 	
-	
+	# Timeout before a display is considered as non-responsive
 	Timeout = 5 #minutes
 
 	include ModelAuthorization
 	
+	# Delegations to the display state object, mostly for legacy reasons
 	delegate :last_contact_at, :last_contact_at=,									to: :display_state, allow_nil: true
-	delegate :last_hello, :last_hello=,											to: :display_state, allow_nil: true
+	delegate :last_hello, :last_hello=,														to: :display_state, allow_nil: true
 	delegate :websocket_connection_id, :websocket_connection_id=, to: :display_state, allow_nil: true
 	delegate :current_slide_id, :current_slide_id=,								to: :display_state, allow_nil: true
 	delegate :current_group_id, :current_group_id=,								to: :display_state, allow_nil: true
@@ -55,15 +42,17 @@ class Display < ActiveRecord::Base
 	alias_method :queue, :override_queues
 	alias_method :state, :display_state
 	
+	# Used for broadcasting events with callbacks to websocket clients
 	def websocket_channel
 		return "display_" + self.id.to_s
 	end
 	
+	# For callback usage
 	def displays
 		return [self]
 	end
 	
-	#Adds a slide to override queue for the display
+	# Adds a slide to override queue for the display
 	def add_to_override(slide, duration, effect = Effect.first!)
 		oq = self.override_queues.new
 		oq.duration = duration
@@ -73,7 +62,7 @@ class Display < ActiveRecord::Base
 		oq.save!
 	end
 	
-	#Either creates a new display with given name or returns exsisting display
+	# Either creates a new display with given name or returns exsisting display
 	def self.hello(display_name, display_ip = nil, connection_id = nil)
 		display = Display.where(:name => display_name).first_or_create
 		display.ip = display_ip
@@ -85,7 +74,7 @@ class Display < ActiveRecord::Base
 		return display
 	end
 	
-	#Remove shown slide from override
+	# Remove shown slide from override
 	def override_shown(override_id, connection_id = nil)
 		self.transaction do
 			oq = self.override_queues.find(override_id)
@@ -97,7 +86,7 @@ class Display < ActiveRecord::Base
 		end
 	end
 	
-	#Set the current group and slide for the display and log the slide as shown
+	# Set the current group and slide for the display and log the slide as shown
 	def set_current_slide(group_id, slide_id, connection_id = nil)
 		if group_id != -1
 			self.current_group = self.presentation.groups.find(group_id)
@@ -112,12 +101,12 @@ class Display < ActiveRecord::Base
 		self.state.save!
 	end
 
-	#Relation for all monitored displays that are more than Timeout minutes late
+	# Relation for all monitored displays that are more than Timeout minutes late
 	def self.late
 		Display.joins(:display_state).where('display_states.monitor = ? AND last_contact_at < ?', true, Timeout.minutes.ago)
 	end
 	
-	#Is this display more than Timeout minutes late?
+	# Is this display more than Timeout minutes late?
 	def late?
 		if self.last_contact_at
 			return Time.diff(Time.now, self.last_contact_at,'%m')[:diff].to_i > Timeout
@@ -126,20 +115,20 @@ class Display < ActiveRecord::Base
 		end
 	end
 	
-	#Returns the time between the last hello and last contact
-	#Since the first thing a display does is to say hello this
-	#gives the time since last display reboot
+	# Returns the time between the last hello and last contact
+	# Since the first thing a display does is to say hello this
+	# gives the time since last display reboot
 	def uptime
 		return nil unless self.last_hello && self.last_contact_at
 		
 		return Time.diff(self.last_hello, self.last_contact_at, '%h:%m:%s')[:diff]
 	end
 	
-	#Return a hash containing all associated data, including the slides
-	#in the presentation.
+	# Return a hash containing all associated data, including the slides
+	# in the presentation.
 	def to_hash
 		h = Hash.new
-		#Legacy stuff, updated at used to get touched when anything happened
+		# Legacy stuff, updated_at used to get touched when anything happened
 		if self.state_updated_at > self.updated_at
 			h[:updated_at] = self.state_updated_at.to_i
 		else
@@ -166,5 +155,22 @@ class Display < ActiveRecord::Base
 		return h
 	end
 	
+	private
+	
+	# Create the associated display state as needed
+	def create_state
+		if self.display_state.nil?
+			ds = DisplayState.new
+			self.display_state = ds
+		end
+	end
+	
+	# If display is in manual control also stop accepting overrides
+	def manual_control_checks
+		if self.manual
+			self.do_overrides = false
+		end
+		return true
+	end
 
 end
