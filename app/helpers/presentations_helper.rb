b6b0b2132bdfc8d5c26afa32f8d76bc0e4c44542
Vesa-Pekka Palmu <vpalmu@depili.fi>
2014-06-25 23:03:08 +0300

    Whitespace and comments
    Fix indentation and add comments

--- app/helpers/presentations_helper.rb
+++ app/helpers/presentations_helper.rb
@@ -1,13 +1,16 @@
 # ISK - A web controllable slideshow system
 #
-# Author::    Vesa-Pekka Palmu
+# Author::		Vesa-Pekka Palmu
 # Copyright:: Copyright (c) 2012-2013 Vesa-Pekka Palmu
-# License::   Licensed under GPL v3, see LICENSE.md
+# License::		Licensed under GPL v3, see LICENSE.md
 
 
 module PresentationsHelper
-  def duration_to_text(dur)
-    (Time.mktime(0)+dur).strftime("%H:%M:%S")
-  end
+	
+	# Convert the slide duration from seconds into human readable format
+	# FIXME: namespace this properly
+	def duration_to_text(dur)
+		(Time.mktime(0)+dur).strftime("%H:%M:%S")
+	end
 
 end
