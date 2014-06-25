7f4660e2c6b6f6931f464e81c04f949c6d95e307
Vesa-Pekka Palmu <vpalmu@depili.fi>
2014-06-25 23:01:23 +0300

    Minor cleanup
    Fix indentation, add comments, minor style changes

--- app/helpers/groups_helper.rb
+++ app/helpers/groups_helper.rb
@@ -1,20 +1,23 @@
 # ISK - A web controllable slideshow system
 #
-# Author::    Vesa-Pekka Palmu
+# Author::		Vesa-Pekka Palmu
 # Copyright:: Copyright (c) 2012-2013 Vesa-Pekka Palmu
-# License::   Licensed under GPL v3, see LICENSE.md
+# License::		Licensed under GPL v3, see LICENSE.md
 
 
 module GroupsHelper
+	
+	# Cache key for group reated stuff
 	def group_cache_key(group)
 		group.cache_key + current_user.cache_key
 	end
 	
-  def group_link_tag(g)
-    html = 'Group: ' 
-    html << link_to(g.name, group_path(g), {:name => 'group_' + g.id.to_s} )
-    html << " Slides: #{g.slides.published.count}/#{g.slides.count}" 
-    return html.html_safe
-  end
+	# Construct a link to a group with basic group infor
+	def group_link_tag(g)
+		html = 'Group: ' 
+		html << link_to(g.name, group_path(g), { name: "group_#{g.id}" } )
+		html << " Slides: #{g.slides.published.count}/#{g.slides.count}" 
+		return html.html_safe
+	end
 	
 end
