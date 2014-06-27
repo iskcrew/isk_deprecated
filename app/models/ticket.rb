9f5b497046ce3e5739a93d4f5ce8afee5b54fbd3
Vesa-Pekka Palmu <vpalmu@depili.fi>
2014-06-27 20:15:16 +0300

    Assign new tickets to current event

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -8,7 +8,10 @@ class Ticket < ActiveRecord::Base
 	validates :name, presence: true
 	validates :status, inclusion: { in: Ticket::StatusCodes }
 	validates :description, presence: true
+	validates :event, presence: true
 	validate :check_valid_models
+	
+	before_validation :assign_to_current_event, on: :create
 	
 	scope :current, -> { where event_id: Event.current.id }
 	scope :open, -> { where.not status: StatusCodes[:closed] }
@@ -23,6 +26,13 @@ class Ticket < ActiveRecord::Base
 			pass = pass ? true : self.about.is_a?(m)
 		end
 		errors.add(:about, "must be a valid object") unless pass
+	end
+	
+	# Assign the created ticket to the current event
+	def assign_to_current_event
+		if self.event.blank?
+			self.event = Event.current
+		end
 	end
 	
 end
