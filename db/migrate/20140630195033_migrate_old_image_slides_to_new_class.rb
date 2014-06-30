79a02516573eecc32cddd8aca14a587a0ed63428
Vesa-Pekka Palmu <vpalmu@depili.fi>
2014-06-30 23:01:08 +0300

    Migrate old image-slides to new ImageSlide class
    Previously we stored plain bitmap slides as a instance of Slide, now we have a class for them, the ImageSlide class. This new class allows us to have nicer controls about image scaling for the user.

---
+++ db/migrate/20140630195033_migrate_old_image_slides_to_new_class.rb
@@ -0,0 +1,27 @@
+#
+#  20140630195033_migrate_old_image_slides_to_new_class.rb
+#  isk
+#
+#  Created by Vesa-Pekka Palmu on 2014-06-30.
+#  Copyright 2014 Vesa-Pekka Palmu. All rights reserved.
+#
+# Migrate the old-style image slides into ImageSlide class
+# This new class allows the user more options to control the image scaling
+
+class MigrateOldImageSlidesToNewClass < ActiveRecord::Migration
+  def up
+		sql = "UPDATE slides set type = 'ImageSlide' where type is NULL and is_svg is false;"
+		connection = ActiveRecord::Base.connection
+		connection.transaction do
+			connection.execute(sql)
+		end
+  end
+	
+	def down
+		sql = "UPDATE slides set type = NULL, is_svg = false where type = 'ImageSlide';"
+		connection = ActiveRecord::Base.connection
+		connection.transaction do
+			connection.execute(sql)
+		end
+	end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140629153945) do
+ActiveRecord::Schema.define(version: 20140630195033) do
 
   create_table "delayed_jobs", force: true do |t|
     t.integer  "priority",   default: 0
