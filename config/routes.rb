c5d9312cfd0c22fa98580639ddc2e21ce43a516a
Vesa-Pekka Palmu <vpalmu@depili.fi>
2014-07-03 17:09:59 +0300

    Order routes alphabetically.
    Our routes were quite messy, now we have them in nicely alphabetical order.

--- config/routes.rb
+++ config/routes.rb
@@ -55,26 +55,74 @@ Isk::Application.routes.draw do
 	# This is a legacy wild controller route that's not recommended for RESTful applications.
 	# Note: This route will make all actions in every controller accessible via GET requests.
 	# match ':controller(/:action(/:id))(.:format)'
-		
-	resource :login do
+
+	root :to => 'slides#index'
+
+	resources :displays do
+		member do
+			get 'presentation'
+			get 'slide_queue'
+			get 'slide_stats'
+			post 'sort_queue'
+			post 'remove_override'
+			put 'update_override'
+			post 'grant'
+			post 'deny'
+			get 'dpy_control'
+		end
+
+		collection do
+			post 'hello'
+		end
 	end
-	
-	resources :tickets do
+
+	resources :events do
 		member do
 		end
-		
+
+		collection do
+
+		end
+	end
+
+	resources :groups do
+		member do
+			post 'sort', :as => 'sort'
+			get 'add_slides'
+			post 'adopt_slides'
+			post 'hide_all'
+			post 'publish_all'
+			post 'grant'
+			post 'deny'
+			post 'add_to_override'
+			get 'download_slides'
+		end
+	end
+
+	resource :login do
+	end
+
+	resources :presentations do
+		member do
+			post 'sort', :as => 'sort'
+			post 'add_group'
+			post 'remove_group'
+			get 'preview'
+			post 'grant'
+			post 'deny'
+			post 'add_to_override'
+		end
+
 		collection do
 		end
 	end
 
-	resources :users do
+	resources :schedules do
 		member do
-			get 'roles'
-			post 'grant'
 		end
-		
+
 		collection do
-			
+
 		end
 	end
 
@@ -96,85 +144,37 @@ Isk::Application.routes.draw do
 			post 'clone'
 			get 'thumb'
 		end
-		
+
 		collection do
 		end
 	end
 
-	resources :events do
-		member do
-		end
-		
-		collection do
-			
-		end
-	end
-	
-	resources :groups do
-		member do
-			post 'sort', :as => 'sort'
-			get 'add_slides'
-			post 'adopt_slides'
-			post 'hide_all'
-			post 'publish_all'
-			post 'grant'
-			post 'deny'
-			post 'add_to_override'
-			get 'download_slides'
-		end
-	end
-	
-	resources :presentations do
-		member do
-			post 'sort', :as => 'sort'
-			post 'add_group'
-			post 'remove_group'
-			get 'preview'
-			post 'grant'
-			post 'deny'
-			post 'add_to_override'
-		end
-		
-		collection do
-		end
-	end
-	
-	resources :schedules do
-		member do
-		end
-		
-		collection do
-			
-		end
-	end
-	
 	resources :templates do
 		member do
 			post :sort
 		end
-		
+
 		collection do
 		end
 	end
-		
-	resources :displays do
+
+	resources :tickets do
 		member do
-			get 'presentation'
-			get 'slide_queue'
-			get 'slide_stats'
-			post 'sort_queue'
-			post 'remove_override'
-			put 'update_override'
-			post 'grant'
-			post 'deny'
-			get 'dpy_control'			 
 		end
-		
+
 		collection do
-			post 'hello'
 		end
 	end
-	
-	root :to => 'slides#index'
-	
+
+	resources :users do
+		member do
+			get 'roles'
+			post 'grant'
+		end
+
+		collection do
+
+		end
+	end
+
 end
