7f1d6fba25e571e32e55a5ff7b1d833ac37bdec5
Vesa-Pekka Palmu <vpalmu@depili.fi>
2014-07-09 18:49:40 +0300

    Clean up unused config related stuff

--- app/models/event.rb
+++ app/models/event.rb
@@ -26,9 +26,7 @@ class Event < ActiveRecord::Base
 	# Create the associated groups as needed and set their event_id
 	before_validation :create_groups, on: :create
 	after_create :set_group_event_ids
-	
-	FilePath = Rails.root.join 'data', 'events'
-	
+		
 	#Default config for events
 	DefaultConfig = {
 		full: { # Full slide image size
@@ -42,40 +40,6 @@ class Event < ActiveRecord::Base
 		thumb: { # Small thumbnail size
 			width: 128,
 			height: 72
-		},
-		simple_editor: {
-			heading: {
-				x: 30,
-				y: 100
-			},
-			body: {
-				y: 200,
-				margin_left: 30,
-				margin_right: 30
-			}
-		},
-		schedule: {
-			items_per_slide: 9,
-			wrap: {
-				soft: 30,
-				hard: 35
-			},
-			use_positions_from_simple: true,
-			positions: {
-				heading: {
-					x: 30,
-					y: 100,
-					font_size: 80
-				},
-				subheader: {
-					color: '#ffd700'
-				},
-				body: {
-					x: 30,
-					y: 200,
-					font_size: 48
-				}
-			}
 		}
 	}
 		
@@ -136,24 +100,5 @@ class Event < ActiveRecord::Base
 			errors.add(:current, "^Must have one current event")
 		end
 	end
-	
-	def config
-		return @_config ||= read_config
-	end
-
-	def read_config
-    if !self.new_record? && File.exists?(config_filename)
-      config = YAML.load(File.read(config_filename))
-		end
-		return config.blank? ? DefaultConfig : config
-	end
-	
-	def write_config
 		
-	end
-	
-	def config_filename
-		FilePath.join 'event_' + self.id.to_s + '_config.yml'
-	end
-	
 end
