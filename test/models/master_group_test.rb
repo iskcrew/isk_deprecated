d977e10d1bf01888605dd119032cda311548d8a8
Vesa-Pekka Palmu <vpalmu@depili.fi>
2014-08-02 18:45:55 +0300

    Update the test to reflect new data format

--- test/models/master_group_test.rb
+++ test/models/master_group_test.rb
@@ -3,8 +3,11 @@ require 'test_helper'
 class MasterGroupTest < ActiveSupport::TestCase
 
 	test "Create new prize group" do
-		data = [{name: 'Fooo', by: 'Me', pts: '3'},{name: 'Bar', by: 'You', pts: '2'}]
-		
+		awards = [{name: 'Fooo', by: 'Me', pts: '3'},{name: 'Bar', by: 'You', pts: '2'}]
+		data = {
+			title: 'Test awards',
+			awards: awards
+		}
 		pg = PrizeGroup.new(
 			name: 'Competition Competition',
 			data: data
@@ -16,8 +19,8 @@ class MasterGroupTest < ActiveSupport::TestCase
 		
 		assert pg.slides.count == 3, "Prizegroup didn't have correct number of slides"
 		assert pg.name == 'Competition Competition'
-		assert pg.data[0][:name] == 'Fooo'
-		assert pg.data[1][:by] == 'You'
+		assert pg.data[:awards][0][:name] == 'Fooo'
+		assert pg.data[:awards][1][:by] == 'You'
 		
 		pg.slides.each do |s|
 			clear_slide_files(s)
