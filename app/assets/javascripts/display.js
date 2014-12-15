4a0b0baa3a827e79e2e0d5231ee07a66ccc27124
Vesa-Pekka Palmu <vpalmu@depili.fi>
2014-12-15 23:57:56 +0200

    Add basic javascript needed for the web-dpy

---
+++ app/assets/javascripts/display.js
@@ -0,0 +1,26 @@
+// This is a manifest file that'll be compiled into display.js, which will include all the files
+// listed below.
+//
+// This file is used for the webgl based display.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// the compiled file.
+//
+// WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
+// GO AFTER THE REQUIRES BELOW.
+//
+// Jquery and plugins
+//= require jquery
+//= require jquery_ujs
+//= require jquery.ui.all
+//= require jquery.timer
+//= require jquery-noty.main
+//
+// Websocket javascript client and instance bound to window
+//= require websocket_rails/main
+//
+// three.js and others
+//* require_tree ./display/
