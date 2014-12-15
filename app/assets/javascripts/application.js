a6a1c134a6d519040a7d6639f84829c1e9f9ff8c
Vesa-Pekka Palmu <vpalmu@depili.fi>
2014-12-15 23:45:40 +0200

    Tidy up javascript hierarchy
    We will soon need two different javascript blobs, one for the web-gui and one for the webgl based display.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,12 +10,15 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
+// Jquery and plugins
 //= require jquery
 //= require jquery_ujs
 //= require jquery.ui.all
 //= require jquery.timer
 //= require jquery-noty.main
+// Websocket javascript client and instance bound to window
 //= require websocket_rails/main
 //= require init_dispatcher
-//= require_tree .
+// All javascript in the "main" directory
+//= require_tree ./main
 
