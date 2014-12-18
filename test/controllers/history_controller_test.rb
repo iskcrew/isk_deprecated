428a9ea5adbc1a6aa2e913592b209a52b1ba2400
Vesa-Pekka Palmu <vpalmu@depili.fi>
2014-12-19 00:02:15 +0200

    Add tests for the new history controller

--- test/controllers/history_controller_test.rb
+++ test/controllers/history_controller_test.rb
@@ -1,7 +1,29 @@
 require 'test_helper'
 
 class HistoryControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+		@adminsession = {user_id: users(:admin).id, username: users(:admin).username}
+	end
+	
+	test "get empty history" do
+		d = displays(:normal)
+		get :index, {display_id: d.id}, @adminsession
+		assert_response :success
+	end
+	
+	test "get history with few entries" do
+		d = displays(:with_display_counts)
+		get :index, {display_id: d.id}, @adminsession
+		assert_response :success
+	end
+	
+	test "clear history" do
+		d = displays(:with_display_counts)
+		assert_difference "DisplayCount.count", -d.display_counts.count do
+			post :clear, {display_id: d.id}, @adminsession
+			assert_redirected_to display_history_index_path(d)
+		end
+	end
+
 end
