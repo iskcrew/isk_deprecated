2edc937a06de1374a4708f74c0055f588b669a47
Vesa-Pekka Palmu <vpalmu@depili.fi>
2014-12-28 12:47:10 +0200

    Clear up dead code

--- app/helpers/admin_helper.rb
+++
@@ -1,9 +0,0 @@
-# ISK - A web controllable slideshow system
-#
-# Author::    Vesa-Pekka Palmu
-# Copyright:: Copyright (c) 2012-2013 Vesa-Pekka Palmu
-# License::   Licensed under GPL v3, see LICENSE.md
-
-
-module AdminHelper
-end
