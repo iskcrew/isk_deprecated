aaa6a5a1452382f1f0c216888691421360232426
Vesa-Pekka Palmu <vpalmu@depili.fi>
2014-12-28 21:30:05 +0200

    Use consistent icons and button text

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -6,4 +6,26 @@
 
 
 module UsersHelper
+	
+	# Buttons
+	def user_assign_roles_button(user)
+		link_to icon('plus', 'Assign roles'), roles_user_path(user), class: 'button'
+	end
+	
+	def user_edit_button(user)
+		link_to icon('edit', 'Edit'), edit_user_path(user), class: 'button'
+	end
+	
+	def user_delete_button(user)
+		options = {
+			method: :delete, 
+			class: 'button warning',
+			data: {confirm: "Are you sure you want to delete this user?"}
+		}
+		link_to icon('times-circle', 'Delete'), user_path(user), options 
+	end
+	
+	def user_details_button(user)
+		link_to icon('info-circle', 'Details'), user_path(user), class: 'button'
+	end
 end
