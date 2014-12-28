9f00d1f32960753d4998ca3426671d2671a26f1f
Vesa-Pekka Palmu <vpalmu@depili.fi>
2014-12-28 13:10:08 +0200

    Migrate the template to slim
    Also clean up the code and add helpers for action buttons for events

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -7,6 +7,24 @@
 
 module EventsHelper
 	
+	# Buttons
+	def event_edit_button(event)
+		link_to icon('edit', 'Edit'), edit_event_path(event), class: 'button'
+	end
+	
+	def event_details_button(event)
+		link_to icon('info-circle', 'Details'), event_path(event), class: 'button'
+	end
+	
+	def event_delete_button(event)
+		options = {
+			method: :delete, 
+			class: 'button warning',
+			data: {confirm: 'Are you sure you want to delete this event?'}
+			}
+		link_to icon('times-circle', 'Delete'), event_path(event), options
+	end
+	
 	# Select box for choosing the slide resolution for the event.
 	def event_slide_resolution_select(event)
 		resolutions = Event::SupportedResolutions
