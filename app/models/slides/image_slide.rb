a3a3e40034e829805406b6f16acfe71ed1b434f3
Vesa-Pekka Palmu <vpalmu@depili.fi>
2015-01-03 00:53:22 +0200

    Migrate generate_Full_image from rmagick to cli call
    rmagick gem takes up obscene amounts of memory, so we are migrating the few things we do with it to CLI calls.

--- app/models/slides/image_slide.rb
+++ app/models/slides/image_slide.rb
@@ -40,46 +40,34 @@ class	ImageSlide < Slide
 	
 	# Generate the full size slide preview
 	def generate_full_image
-		# Read the uploaded original image
-		picture = Magick::ImageList.new(self.original_filename).first
-		
 		bg_color = self.slidedata[:background]
 		scale = self.slidedata[:scale]
-		size = picture_sizes[:full]
+		size = picture_sizes[:full].join('x')
 		
 		# Build the ImageMagick geometry string
 		# The string is WIDTHxHEIGHT + scaling operator as follows
 		# > to scale the image down if its height or width exceed the target
 		# < will scale the image up if its height and width are smaller than the target
 		# ! will scale to fit breaking aspect ratio
-		geo_str = size.join('x')
+		geo_str = size
 		case scale
 		when 'down'
 			# Scale the image down if needed
-			geo_str << '>'
+			geo_str << '\>'
 		when 'fit'
 			# Scale the image to fit maintaining aspect ratio
 			# Nothing to do
 		when 'up'
 			# Only scale the image up if needed
-			geo_str << '<'
+			geo_str << '\<'
 		when 'stretch'
 			# Stretch the image to fill the entire slide disregarding aspect ratio
 			geo_str << '!'
 		end
-		
-		# Scale the image
-		picture = picture.change_geometry!(geo_str) do |cols, rows, img|
-			# resize our image
-			img.resize!(cols, rows)
-			# build the background
-			bg = Magick::Image.new(*size) { self.background_color = bg_color }
-			# center the image on our new background
-			bg.composite(img, Magick::CenterGravity, Magick::OverCompositeOp)
- 		end
-		
-		picture.write(self.full_filename)
-		
+	
+		command = "convert #{self.original_filename} -resize #{geo_str}"
+		command << " -background '#{bg_color}' -gravity center -extent #{size} #{self.full_filename}"
+		system command	
 	end
 	
 end
