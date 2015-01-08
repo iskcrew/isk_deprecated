7b03bc1d6c684fa1a430563db03a022ef52265ac
Vesa-Pekka Palmu <vpalmu@depili.fi>
2015-01-08 22:05:17 +0200

    Define session cookie lifetime
    Without the expires_after definition the session cookie will expire after the current session as per RFC. We want our displays to have longer lived sessions so that they don't need to reauthenticate on restarts.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Isk::Application.config.session_store :cookie_store, :key => '_isk_session'
+Isk::Application.config.session_store :cookie_store, key: '_isk_session', expire_after: 2.weeks
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
