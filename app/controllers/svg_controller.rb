b315189a456737d65bc2b7376d83e1b622f43a84
Vesa-Pekka Palmu <vpalmu@depili.fi>
2015-01-12 22:37:26 +0200

    Comments

--- app/controllers/svg_controller.rb
+++ app/controllers/svg_controller.rb
@@ -1,24 +1,21 @@
 # ISK - A web controllable slideshow system
 #
 # Author::		Vesa-Pekka Palmu
-# Copyright:: Copyright (c) 2012-2013 Vesa-Pekka Palmu
+# Copyright:: Copyright (c) Vesa-Pekka Palmu
 # License::		Licensed under GPL v3, see LICENSE.md
-
 
 class SvgController < WebsocketRails::BaseController
 	# This is a controller for svg related websocket actions
-	# Currently we only support generating a new svg with
-	# the simple_slide svg generator
-	
+
+	# Generate the svg for a simple slide, used for the browser preview
 	def simple
 		svg = SimpleSlide.create_svg(message[:simple])
 		trigger_success svg
 	end
 	
+	# Generate the svg for a templateslide, used for the browser preview
 	def template
 		svg = SlideTemplate.find(message[:template_id]).generate_svg(message)
 		trigger_success svg
 	end
-
-
 end
