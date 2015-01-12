68d993ea99b73c8295c457bf7815599ae180d4c3
Vesa-Pekka Palmu <vpalmu@depili.fi>
2015-01-12 21:54:05 +0200

    Code style cleanup
    Organize the methods, add comments, clean indentation and use the new style hashes

--- app/controllers/displays_controller.rb
+++ app/controllers/displays_controller.rb
@@ -1,21 +1,25 @@
 # ISK - A web controllable slideshow system
 #
-# Author::    Vesa-Pekka Palmu
+# This controller handles the managing of remote isk displays.
+#
+# Author::		Vesa-Pekka Palmu
 # Copyright:: Copyright (c) 2012-2013 Vesa-Pekka Palmu
-# License::   Licensed under GPL v3, see LICENSE.md
+# License::		Licensed under GPL v3, see LICENSE.md
 
 
 class DisplaysController < ApplicationController
-	
 	# ACL filters
-  before_action :require_create, :only => [:new, :create]
-    
-  def index
-    @displays = Display.order(:name)
-    
-    respond_to do |format|
-      format.js
-      format.html
+	before_action :require_create, :only => [:new, :create]
+	
+	# List all displays
+	# We support html or json for the whole list and
+	# js for updating the late display warnings on pages.
+	def index
+		@displays = Display.order(:name)
+		
+		respond_to do |format|
+			format.js
+			format.html
 			format.json {
 				render json: @displays.collect { |d|
 					{
@@ -25,26 +29,28 @@ class DisplaysController < ApplicationController
 						late: d.late?}
 					}
 			}
-    end
-    
-  end
-    
-  def show
-    @display = Display.includes(:presentation, :override_queues => :slide).find(params[:id])
-    
-    respond_to do |format|
-      format.html 
-      format.json {
-        render(:json =>JSON.pretty_generate(@display.to_hash))
-      }
-      format.js
-    end
-  end
-  
-  def slide_stats
-    @display = Display.find(params[:id])
-    
-  end
+		end
+	end
+	
+	# Get the details on a given display
+	# We support html, json for the data serilazation and
+	# js for updating the div#display_<id> blocks
+	def show
+		@display = Display.includes(:presentation, override_queues: :slide).find(params[:id])
+		
+		respond_to do |format|
+			format.html 
+			format.json {
+				render json: JSON.pretty_generate(@display.to_hash)
+			}
+			format.js
+		end
+	end
+	
+	# Get the 
+	def slide_stats
+		@display = Display.find(params[:id])
+	end
 	
 	# Delete a display and associated data
 	def destroy
@@ -56,55 +62,68 @@ class DisplaysController < ApplicationController
 		flash[:notice] = "Deleted display id: #{display.id} - #{display.name}"
 		redirect_to displays_path
 	end
-  
-  def new
-    
-  end
-  
-  def create
-    
-  end
-  
-  def edit
-    @display = Display.find(params[:id])
-    require_edit @display
-  end
-  
-  def update_override
-    oq = OverrideQueue.find(params[:id])
-    require_edit oq.display
-    
-    oq.duration = params[:override_queue][:duration]
-    oq.save!
-    flash[:notice] = "Duration was changed"
-    redirect_to :back
-  end
-  
-  def update
-    @display = Display.find(params[:id])
-    require_edit @display
+	
+	# FIXME: allow creation of displays from the web gui
+	def new
+		
+	end
+	
+	def create
+		
+	end
+	
+	# Render the edit form for a given display
+	def edit
+		@display = Display.find(params[:id])
+		require_edit @display
+	end
+	
+	# Update a given display
+	# We support html and js, that updates the div#display_<id> block
+	def update
+		@display = Display.find(params[:id])
+		require_edit @display
 
-    if @display.update_attributes(display_params)
-	    respond_to do |format|
-	      format.html {
+		if @display.update_attributes(display_params)
+			respond_to do |format|
+				format.html {
 					flash[:notice] = 'Display was successfully updated.'
 					redirect_to :action => :show, :id => @display.id
 				}
-	      format.js {render :show}
-	    end
-    else
-      flash[:error] = "Error updating display."
-      render :action => 'edit' and return
-    end    
-  end  
-
-  #Remote control for iskdpy via javascript and websockets
-  def dpy_control
-    @display = Display.find(params[:id])
-  end
-        	
+				format.js {render :show}
+			end
+		else
+			flash[:error] = "Error updating display."
+			render :action => 'edit' and return
+		end		 
+	end	 
+	
+	# FIXME: create a nested override controller
+	def update_override
+		oq = OverrideQueue.find(params[:id])
+		require_edit oq.display
+		
+		oq.duration = params[:override_queue][:duration]
+		oq.save!
+		flash[:notice] = "Duration was changed"
+		redirect_to :back
+	end
+	
+	def remove_override
+		oq = OverrideQueue.find(params[:id]).destroy
+		require_override oq.display
+		
+		flash[:notice] = 'Removed slide from override queue'
+		redirect_to :back
+	end
+	
+	#Remote control for iskdpy via javascript and websockets
+	def dpy_control
+		@display = Display.find(params[:id])
+	end
+					
 	#FIXME: this logic needs to go to the model
-  def sort_queue
+	def sort_queue
 		@display = Display.find(params[:id])
 		require_edit @display
 		
@@ -117,50 +136,43 @@ class DisplaysController < ApplicationController
 			end
 		else
 			render :text => "Invalid request, try refreshing", :status => 400
-		end		    
-  end
-  
-  def remove_override
-    oq = OverrideQueue.find(params[:id]).destroy
-    require_override oq.display
-    
-    flash[:notice] = 'Removed slide from override queue'
-    redirect_to :back
-  end
-  
-  def deny
-    display = Display.find(params[:id])
-    user = User.find(params[:user_id])
-    display.authorized_users.delete(user)
-    redirect_to :back
-  end
-  
-  def grant
-    display = Display.find(params[:id])
-    user = User.find(params[:grant][:user_id])
-    display.authorized_users << user
-    redirect_to :back    
-  end
-  
-  
-  private
+		end				
+	end
 	
+	# FIXME: the ACL stuff needs to go to nested controller
+	def deny
+		display = Display.find(params[:id])
+		user = User.find(params[:user_id])
+		display.authorized_users.delete(user)
+		redirect_to :back
+	end
+	
+	def grant
+		display = Display.find(params[:id])
+		user = User.find(params[:grant][:user_id])
+		display.authorized_users << user
+		redirect_to :back		 
+	end
+	
+	private
+	
+	# Whitelist the parameters for updating displays
 	def display_params
 		params.required(:display).permit(:name, :presentation_id, :manual, :monitor, :do_overrides)
 	end
-  
-  def require_admin
-    raise ApplicationController::PermissionDenied unless Display.admin? current_user
-  end
-  
-  def require_create
-    raise ApplicationController::PermissionDenied unless Display.can_create? current_user
-  end
-  
-  def require_override(d)
-    raise ApplicationController::PermissionDenied unless d.can_override? current_user
-  end
- 
-
-
+	
+	# Require display admin priviledges or raise PermissionDenied
+	def require_admin
+		raise ApplicationController::PermissionDenied unless Display.admin? current_user
+	end
+	
+	# Require display create permission or raise PermissionDenied
+	def require_create
+		raise ApplicationController::PermissionDenied unless Display.can_create? current_user
+	end
+	
+	# Require permission to add slides to the override queue on a display
+	def require_override(d)
+		raise ApplicationController::PermissionDenied unless d.can_override? current_user
+	end
 end
