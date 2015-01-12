c8119babd64870aa2cd19559434d55fe5bffae62
Vesa-Pekka Palmu <vpalmu@depili.fi>
2015-01-12 22:18:33 +0200

    Header and comments

--- app/controllers/history_controller.rb
+++ app/controllers/history_controller.rb
@@ -1,12 +1,23 @@
+# ISK - A web controllable slideshow system
+#
+# Author::		Vesa-Pekka Palmu
+# Copyright:: Copyright (c) Vesa-Pekka Palmu
+# License::		Licensed under GPL v3, see LICENSE.md
+
 class HistoryController < ApplicationController
 	
+	# Get the display history for a given display or slide
+	# Since this controller is nested on slides and displays we
+	# can deal with both cases here
 	def index
 		if params[:display_id].present?
+			# We show all slides shown on given display
 			@display = Display.find(params[:display_id])
 			@all_slides = @display.display_counts.joins(:slide).order('count_all desc')
 				.group("slides.name", "slides.id").count
 			render :index_by_display and return
 		else
+			# We show when the given slide has been shown
 			@slide = Slide.find(params[:slide_id])
 			@displays = @slide.display_counts.joins(:display).order('count_all desc')
 				.group("displays.name", "displays.id").count
@@ -14,12 +25,14 @@ class HistoryController < ApplicationController
 		end
 	end
 	
+	# Show detailed information when a given slide has been shown on a given display
 	def show
 		@display = Display.find(params[:display_id])
 		@slide = Slide.find(params[:id])
 		@display_counts = @display.display_counts.where(slide_id: @slide.id).order(updated_at: :asc)
 	end
 	
+	# Clear the history for a given display
 	def clear
 		display = Display.find(params[:display_id])
 		
