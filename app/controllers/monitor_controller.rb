d16dc7f990f4ebd70bef8077ded084f0027f6d08
Vesa-Pekka Palmu <vpalmu@depili.fi>
2015-01-12 22:27:16 +0200

    Add header

--- app/controllers/monitor_controller.rb
+++ app/controllers/monitor_controller.rb
@@ -1,3 +1,9 @@
+# ISK - A web controllable slideshow system
+#
+# Author::		Vesa-Pekka Palmu
+# Copyright:: Copyright (c) Vesa-Pekka Palmu
+# License::		Licensed under GPL v3, see LICENSE.md
+
 class MonitorController < ApplicationController
 	
 	# Websocket + html5 text-to-speech monitoring interface
