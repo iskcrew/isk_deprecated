81c9e95588e61f3eb98bf3add4556f18ae674edd
Vesa-Pekka Palmu <vpalmu@depili.fi>
2015-01-31 12:18:54 +0200

    Create schedule slides with nokogiri
    We ditch the old hard-to-maintain erb template and instead build the schedules on top of the simple slide svg generator.

--- app/models/slides/schedule_slide.rb
+++ app/models/slides/schedule_slide.rb
@@ -8,7 +8,63 @@
 class ScheduleSlide < SvgSlide
 	#Automatically generated schedule slide
 	TypeString = 'schedule'
+	
+	# FIXME: proper settings
+	SubheaderX = 50
+	SubheaderFill = '#e2e534'
+	TimeX = SubheaderX + 50
+	ItemNameX = TimeX + 230
+	ItemSpacing = 80
+	FontSize = '72px'
+	
+	
+	
+	# Create the slide svg from passed schedule events
+	def create_svg(header, items)
+		self.name = header
+		svg = Nokogiri::XML(SimpleSlide.create_svg({heading: header}))
+		
+		body = svg.at_css(SimpleSlide::BodySelector)
+		body.children.each do |c|
+			c.remove
+		end
+
+		body['sodipodi:linespacing'] = '100%'
+		
+		items.each do |item|
+			row = Nokogiri::XML::Node.new 'tspan', body
+			row['sodipodi:role'] = 'line'
+			row['font-size'] = FontSize
+			
+			if item[:subheader]
+				tspan = Nokogiri::XML::Node.new 'tspan', row
+				tspan['x'] = SubheaderX
+				tspan['fill'] = SubheaderFill
+				tspan.content = item[:subheader]
+				row.add_child tspan
+			else
+				# Time
+				tspan_time = Nokogiri::XML::Node.new 'tspan', row
+				tspan_time['x'] = TimeX
+				tspan_time.content = item[:time]
+				row.add_child tspan_time
+				
+				# name
+				tspan_name = Nokogiri::XML::Node.new 'tspan', row
+				tspan_name['x'] = ItemNameX
+				tspan_name.content = item[:name]
+				row.add_child tspan_name
+			end
+			body.add_child row
+		end
+		
+		self.svg_data = svg.to_xml
+	end
 
 	private
+
+	def schedule_row(row, item)
+		
+	end
 
 end
