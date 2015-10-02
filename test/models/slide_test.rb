3b1242e9cdf5369847fdc68f4dc763a908fe4247
Vesa-Pekka Palmu <vpalmu@depili.fi>
2015-10-02 14:17:52 +0300

    Basic tests for the image processing pipeline
    We now do a simple test to see that both ImageSlide and InkscapeSlide types don't introduce any artifacts into the output images.
    
    For inscape slide we use a svg with base dimensions of 16 by 9 points and see that a 1920x1080 bitmap background comes out pixel perfect into a 1920x1080 export from that svg. For imageslide we just check that the same test pattern scaled to fit by imagemagick comes out pixel perfect.

--- test/models/slide_test.rb
+++ test/models/slide_test.rb
@@ -1,7 +1,41 @@
 require 'test_helper'
 
 class SlideTest < ActiveSupport::TestCase
-	test "Test STI inheritance selectors" do
+	def setup
+		Slide.send(:remove_const, :FilePath)
+		Slide.const_set(:FilePath, Rails.root.join('tmp','test'))
+		
+		@testpattern_1080p = Rails.root.join('test', 'assets', 'testpattern_1080p.png').to_s
+		@svg_scaling_test = Rails.root.join('test', 'assets', 'scaling_test.svg').to_s
+	end
+	
+	def teardown
+		Slide.all.each do |s|
+			clear_slide_files(s)
+		end
+	end
+	
+	test "STI inheritance selectors" do
 		assert	SvgSlide.count == 3, "Should have 3 slides inheriting from SvgSlide"
+	end
+	
+	test "imageslide scaling" do
+		slide = ImageSlide.new(name: 'Scaling test')
+		slide.image = File.open(@testpattern_1080p)
+		slide.save!
+		slide.generate_images
+		
+		command = "compare #{@testpattern_1080p} #{slide.full_filename} /dev/null"
+		assert system(command), "Image differs after imageslide processing"
+	end
+	
+	test "Inkscape slide background image scaling" do
+		slide = InkscapeSlide.new(name: 'Scaling test')
+		slide.svg_data = File.read(@svg_scaling_test)
+		slide.save!
+		slide.generate_images
+		
+		command = "compare #{@testpattern_1080p} #{slide.full_filename} /dev/null"
+		assert system(command), "Image differs after inkscape slide processing"
 	end
 end
