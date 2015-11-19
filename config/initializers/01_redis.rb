56513a883b0dca72298f842e5761e5b41d61ff41
Vesa-Pekka Palmu <vpalmu@depili.fi>
2015-11-19 19:19:14 +0200

    Use different redis db on test environment

--- config/initializers/01_redis.rb
+++ config/initializers/01_redis.rb
@@ -1,6 +1,10 @@
 # We have several libraris (cashier,websocket-rails,resque) using redis
 # So set the redis for all of them here
-Rails.application.config.x.redis = {host: 'localhost', port: 6379}
+unless Rails.env.test?
+	Rails.application.config.x.redis = {host: 'localhost', port: 6379}
+else
+	Rails.application.config.x.redis = {host: 'localhost', port: 6379, db: 2}
+end
 
 # Create a redis connection pool for cross-process messages
 $redis_pool = ConnectionPool.new(size: 20, timeout: 5) { Redis.new(Rails.configuration.x.redis) }
