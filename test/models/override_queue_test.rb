24127ab11d56c8471cb49fb6a438d480983ed77f
Vesa-Pekka Palmu <vpalmu@depili.fi>
2015-11-20 18:34:01 +0200

    Test websocket notifications

--- test/models/override_queue_test.rb
+++ test/models/override_queue_test.rb
@@ -1,7 +1,36 @@
 require 'test_helper'
+require 'redis_test_helpers'
 
 class OverrideQueueTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	include RedisTestHelpers
+	
+	test "notifications on override update" do
+		oq = override_queues(:override_1)
+		with_redis(oq.display.websocket_channel) do
+			oq.duration = 500
+			oq.save!
+		end
+		assert_one_isk_message('display', 'data')
+	end
+	
+	test "notifications on override delete" do
+		oq = override_queues(:override_2)
+		with_redis(oq.display.websocket_channel) do
+			oq.destroy
+		end
+		assert_one_isk_message('display', 'data')
+	end
+	
+	test "notifications on override creation" do
+		d = displays(:normal)
+		s = slides(:simple)
+		oq = OverrideQueue.new
+		oq.display = d
+		oq.slide = s
+		oq.duration = 32
+		with_redis(d.websocket_channel) do
+			assert oq.save
+		end
+		assert_one_isk_message('display', 'data')
+	end
 end
