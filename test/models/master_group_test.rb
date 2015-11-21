f35fcc0fd6c870616ac0a863d36b9c3a76eecf7c
Vesa-Pekka Palmu <vpalmu@depili.fi>
2015-11-21 17:52:42 +0200

    Test websocket notifications

--- test/models/master_group_test.rb
+++ test/models/master_group_test.rb
@@ -1,7 +1,9 @@
 require 'test_helper'
+require 'redis_test_helpers'
 
 class MasterGroupTest < ActiveSupport::TestCase
-
+	include RedisTestHelpers
+	
 	test "Create new prize group" do
 		skip('Needs a template in test assets to work')
 		
@@ -37,5 +39,34 @@ class MasterGroupTest < ActiveSupport::TestCase
 		end
 	end
 
-
+	test "notifications on create" do
+		g = MasterGroup.new(name: 'test groups')
+		with_redis do
+			assert g.save
+		end
+		assert_one_isk_message('mastergroup', 'create')
+	end
+	
+	test "notification on update" do
+		g = master_groups(:ten_slides)
+		with_redis do
+			g.name = 'updated name'
+			assert g.save
+		end
+		assert_one_isk_message('mastergroup', 'update')
+	end
+	
+	test "notifications on delete" do
+		mg = master_groups(:ten_slides)
+		slides_count = mg.slides.count
+		with_redis do
+			mg.destroy
+		end
+		assert_equal slides_count, redis_messages.count, "Should trigger #{slides_count} notifications about slides"
+		redis_messages.each do |m|
+			assert msg = IskMessage.from_json(m)
+			assert_equal 'slide', msg.object
+			assert_equal 'update', msg.type
+		end
+	end
 end
