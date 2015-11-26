5689a2614b4eb5736d09177ffdc7bb06d968f250
Vesa-Pekka Palmu <vpalmu@depili.fi>
2015-11-26 11:42:56 +0200

    Add logging to dev environment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -12,6 +12,9 @@ Isk::Application.configure do
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
+	
+	# See everything in the log (default is :info)
+	config.log_level = :info
 
   # Show full error reports and disable caching
   config.consider_all_requests_local       = true
