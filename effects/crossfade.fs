02f23b46049b72f93850ee416390864bcba6ebac
deram <deram@iki.fi>
2015-12-21 00:23:23 +0200

    cleaning up fragment shaders

--- effects/crossfade.fs
+++ effects/crossfade.fs
@@ -6,6 +6,6 @@ uniform float transition_time;
 varying vec2 vUv;
 
 void main() {
-    gl_FragColor = mix(texture2D(from, vec2(vUv)), texture2D(to, vec2(vUv)), transition_time);
+    gl_FragColor = mix(texture2D(from, vUv), texture2D(to, vUv), transition_time);
 }
 
