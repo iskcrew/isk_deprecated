6cd946bc67f24955a6ac12a32ad8333781cc0174
Vesa-Pekka Palmu <vpalmu@depili.fi>
2016-08-01 20:23:56 +0300

    Make room to store ipv6 addresses

--- test/models/display_state_test.rb
+++ test/models/display_state_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class DisplayStateTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	test "ipv6 address" do
+		ds = DisplayState.new
+		ds.monitor = false
+		ds.status = "disconnected"
+		ds.ip = "ABCD:ABCD:ABCD:ABCD:ABCD:ABCD:ABCD:ABCD"
+		assert ds.save, "Error saving DisplayState with ipv6 address."
+		
+		ds.ip = "ABCD:ABCD:ABCD:ABCD:ABCD:ABCD:192.168.158.190"
+		assert ds.save, "Error saving DisplayState with ipv4 mapped ipv6 address"
+	end
 end
