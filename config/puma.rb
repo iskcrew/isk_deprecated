09a5898c6243ec0f48f73ff23c30c1d16a3d0fe9
Vesa-Pekka Palmu <vpalmu@depili.fi>
2017-02-02 10:40:07 +0200

    Logging to console on dev environment

--- config/puma.rb
+++ config/puma.rb
@@ -13,15 +13,15 @@ if (ENV['RAILS_ENV'] || 'development') != 'development'
 
 	# Run as daemon
 	daemonize true
+	
+	# Log puma startup etc.
+	stdout_redirect 'log/puma.log', 'log/puma_error.log', true
 else
 	daemonize false
 	quiet false
 end
 pidfile "tmp/pids/server.pid"
 state_path 'tmp/pids/puma.state'
-
-# Log puma startup etc.
-stdout_redirect 'log/puma.log', 'log/puma_error.log', true
 
 on_worker_boot do
   # Worker specific setup for Rails 4.1+
