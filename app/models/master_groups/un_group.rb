f10b7ebb319326a6eb0883bd80b80660a02cc40b
Vesa-Pekka Palmu <vpalmu@depili.fi>
2017-03-09 21:30:32 +0200

    Initial rubocop pass on master group sti classes

--- app/models/master_groups/un_group.rb
+++ app/models/master_groups/un_group.rb
@@ -6,18 +6,15 @@
 # Copyright:: Copyright (c) 2012-2013 Vesa-Pekka Palmu
 # License::   Licensed under GPL v3, see LICENSE.md
 
+class UnGroup < MasterGroup
+  belongs_to :event
+  has_many :slides, -> { order id: :asc }, foreign_key: :master_group_id
 
-class	UnGroup < MasterGroup
-	belongs_to :event
-	
-  has_many :slides, -> {order id: :asc}, foreign_key: :master_group_id
-	
-	after_initialize do |g|
-		g.internal = true
-	end
+  after_initialize do |g|
+    g.internal = true
+  end
 
-	def name
-		'Ungrouped slides'
-	end
-	
+  def name
+    "Ungrouped slides"
+  end
 end
