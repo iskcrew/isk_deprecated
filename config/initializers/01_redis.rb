dde971f295a54a62be0b475b8537cf23055a1a5b
Vesa-Pekka Palmu <vpalmu@depili.fi>
2017-05-21 00:15:00 +0300

    Style/UnlessElse cop

--- config/initializers/01_redis.rb
+++ config/initializers/01_redis.rb
@@ -1,9 +1,9 @@
 # We have several gems (cashier, resque) using redis
 # So set the redis for all of them here
-unless Rails.env.test?
-  Rails.application.config.x.redis = { host: "localhost", port: 6379 }
-else
+if Rails.env.test?
   Rails.application.config.x.redis = { host: "localhost", port: 6379, db: 2 }
+else
+  Rails.application.config.x.redis = { host: "localhost", port: 6379 }
 end
 
 # Create a redis connection pool for cross-process messages
