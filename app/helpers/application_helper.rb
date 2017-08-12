5acb21018fe3160d41ade3a49841e4063a477a1f
Vesa-Pekka Palmu <vpalmu@depili.fi>
2017-08-12 20:30:11 +0300

    BUGFIX: inactive_toggle
    We tried to modify (now) frozen string literal.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -33,13 +33,8 @@ module ApplicationHelper
 
   # Inactive toggle button with "led"
   def inactive_toggle(name, status)
-    if status
-      html = "<a class='button inactive led green'>"
-    else
-      html = "<a class='button inactive led off'>"
-    end
-    html << name << "</a>"
-    return html.html_safe
+    led = (status ? "green" : "off")
+    "<a class='button inactive led #{led}'>#{name}</a>".html_safe
   end
 
   # Active toggle button with a "led"
